c0a4b7e074cc04442a2390d1d69df358c2055aa8
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-10-25 14:49:04 +0100

    bbb

--- Open-Lab-03.06/Checker.cs
+++ Open-Lab-03.06/Checker.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._06
     {
         public bool HasSpaces(string str)
         {
-            throw new NotImplementedException();
+            bool aaa = false;
+            if (str.Contains(" "))
+            { aaa = true; };
+            return aaa;
         }
     }
 }
